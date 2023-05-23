5b3f0da812aca96a74c3b08b87b651ea3ad74bcc
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-23 10:37:12 +0200

    initated variables and add evenlistener on buttons / added custom hover effect on btn and resize and debug some the sizing issues

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timeRef = document.querySelector(".time");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+  console.log("start");
+});
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+  console.log("pause");
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+  console.log("reset");
+});
